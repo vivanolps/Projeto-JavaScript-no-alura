27481dbac9989de7c50df86b7ac4c0204b80b29f
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-19 11:34:06 -0300

    Fiz as condições no meu código, e deixei acessivel para o teclado.

--- main.js
+++ main.js
@@ -6,13 +6,26 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 // Para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador];
+  const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-   const idAudio = `#som_${instrumento}`;  //template string
+    const idAudio = `#som_${instrumento}`;  //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio); // função anonima
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+        
+        if (evento.code == 'Space') {
+            tecla.classList.add('ativa');  
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
   
 }
