d1710242b08d27cc6774b6d470bffeadc83227ad
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-21 08:31:41 -0300

    Adicionei condições, e melhorei o código.

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function tocaSom (idElementoAudio) {
-     document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+     document.querySelector(seletorAudio);
+
+     if (elemento === null) {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado');
+     }
+
+     if (elemento != null) {
+
+        if (elemento.localName === 'audio') {
+            elemento.play();
+        }
+     }
 }
     
 const listaDeTeclas = document.querySelectorAll('.tecla');
